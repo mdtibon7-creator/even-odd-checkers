e5267997c84973c3c6c3cef5ce82ae4aee292116
mdtibon7-creator <mdtibon7@gmail.com>
2026-01-30 00:54:10 +0800

    Update even-odd-checkers.cpp

--- even-odd-checkers.cpp
+++ even-odd-checkers.cpp
@@ -5,16 +5,14 @@ int main() {
     int number;
 
     cout << "Enter a number: ";
-    if (!(cin >> number)) {
-        cout << "Invalid input. Please enter an integer.";
-        return 0;
-    }
+    cin >> num;
+    
 
-    if (number % 2 == 0) {
+    if (number % 2 == 0) 
         cout << "The number is Even";
-    } else {
+     else 
         cout << "The number is Odd";
-    }
+    
 
     return 0;
 }
